1f7b0f3ea7eaec56e8d1da511577b921a6dafa78
VHeroineV <vheroinev@mail.ru>
2022-07-14 15:38:20 +0300

    Added all vital features.

--- ImageViewer.py
+++ ImageViewer.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt6 import uic, QtGui
+import os
 
 
 class MainGUI(QMainWindow):
@@ -14,6 +15,13 @@ class MainGUI(QMainWindow):
         imagemap = imagemap.scaled(self.width(), self.height())
         self.label.setPixmap(imagemap)
         self.label.setMinimumSize(1, 1)
+        self.file_list = None
+        self.file_counter = None
+        self.actionOpen_Image.triggered.connect(self.open_image)
+        self.actionOpen_Folder.triggered.connect(self.open_directory)
+        self.actionQuit.triggered.connect(self.exit)
+        self.pushButton.clicked.connect(self.previous_image)
+        self.pushButton_2.clicked.connect(self.next_image)
 
     def resizeEvent(self, event):
         try:
@@ -23,13 +31,52 @@ class MainGUI(QMainWindow):
         imagemap = imagemap.scaled(self.width(), self.height())
         self.label.setPixmap(imagemap)
         self.label.resize(self.width(), self.height())
-        
+
+    def exit(self):
+        QApplication.exit()
+
+    def scale_image(self):
+            imagemap = QtGui.QPixmap(self.current_file)
+            imagemap = imagemap.scaled(self.width(), self.height())
+            self.label.setPixmap(imagemap)
+
+    def open_image(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Image Files (*.bmp *.png *.jpg *.jpeg)")
+        if filename != "":
+            self.current_file = filename
+            self.scale_image()
+
+    def next_image(self):
+        if self.file_counter is not None:
+            self.file_counter += 1
+            self.file_counter %= len(self.file_list)
+            self.current_file = self.file_list[self.file_counter]
+            self.scale_image()
+
+    def previous_image(self):
+        if self.file_counter is not None:
+            self.file_counter -= 1
+            self.file_counter %= len(self.file_list)
+            self.current_file = self.file_list[self.file_counter]
+            self.scale_image()
+
+    def open_directory(self):
+        directory = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.file_list = [directory + '/' + f for f in os.listdir(directory) if f.endswith(".jpg") or
+        f.endswith(".jpeg") or f.endswith(".png") or f.endswith(".bmp")]
+        self.file_counter = 0
+        self.current_file = self.file_list[self.file_counter]
+        imagemap = QtGui.QPixmap(self.current_file)
+        imagemap = imagemap.scaled(self.width(), self.height())
+        self.label.setPixmap(imagemap)
+
 
 def main():
     """The main function of application."""
     app = QApplication([])
     window = MainGUI()
     app.exec()
+
 
 if __name__ == "__main__":
     main()
